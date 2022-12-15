2ea6142ed7088f23dfa0f60af165dc5fa35f6c99
Sansegur <119261735+Sansegur@users.noreply.github.com>
2022-12-15 21:28:08 +0100

    Add files via upload

---
+++ PiscineC04/ex00/ft_strlen.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sansegur <sansegur@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/14 16:26:19 by sansegur          #+#    #+#             */
+/*   Updated: 2022/12/14 17:33:59 by sansegur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/*
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+	printf("%i", ft_strlen(argv[1]));
+	return (0);
+}
+*/
---
+++ PiscineC04/ex03/ft_atoi.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sansegur <sansegur@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/15 19:03:38 by sansegur          #+#    #+#             */
+/*   Updated: 2022/12/15 19:24:02 by sansegur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+int	ft_atoi(char *str)
+{
+	int	res;
+	int	sign;
+	int	i;
+
+	res = 0;
+	sign = 1;
+	i = 0;
+	while ((str[i] == 32) || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	while ((str[i] == '-') || (str[i] == '+'))
+	{
+		if (str[i] == '-')
+			sign = sign * -1;
+		i++;
+	}
+	while (str[i] != '\0' && (str[i] >= '0' && str[i] <= '9'))
+	{
+		res = res * 10;
+		res = res + str[i] - 48;
+		i++;
+	}
+	return (res * sign);
+}
